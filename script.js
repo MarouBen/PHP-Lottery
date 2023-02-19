d84a182b5bc27912585f8500279f609a9d1b55c0
BEN ABBOU Marouane <maroumarocom@gmail.com>
2023-02-19 13:03:59 +0100

    added a script that show the numbers selected

--- script.js
+++ script.js
@@ -1,2 +1,6 @@
 var listnum = document.querySelectorAll("#cont .num")
-for (var i = 0; i < listnum.length; i++)
+for (var i = 0; i < listnum.length; i++){
+    listnum[i].addEventListener("click",function(){
+        this.classList.add("selected");
+    });
+}
